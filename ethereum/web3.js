dcb0673c280d39cb391f634e89eb884d74600395
Yoni <ymekuria@gmail.com>
2018-07-11 14:52:40 -0700

    smart contract scaffolding

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -10,10 +10,12 @@ if (typeof window !== 'undefined' && window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // use infura to create a new provider if we are on the server or the user doesn't
-  // jave metamask
+  // have metamask
   const provider = new Web3.providers.HttpProvider(
     'https://rinkeby.infura.io/fYArXFXfUdkoisYsJ7uZ'
   );
 
   web3 = new Web3(provider);
 }
+
+export default web3;
