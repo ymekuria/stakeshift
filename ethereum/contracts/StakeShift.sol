3813e84b9512857845baeb98285d9309903ec27d
Yoni <ymekuria@gmail.com>
2018-08-01 16:48:58 -0700

    refactored from deploying a new smart contract for every agreement to creating a mapping of aggreement structs

--- ethereum/contracts/StakeShift.sol
+++ ethereum/contracts/StakeShift.sol
@@ -13,5 +13,21 @@ contract StakeShift {
     }
 
     mapping(address => Agreement) public agreements; 
+
+    function createAgreement(string agreementDescription, address currentSeller ) public payable {
+        
+        Agreement memory newAgreement = Agreement({
+            description: agreementDescription,
+            buyer: msg.sender,
+            seller: currentSeller,
+            amount: msg.value,
+            buyerApproved: false,
+            sellerApproved: false,
+            isComplete: false
+        });
+        
+        agreements[msg.sender] = newAgreement;
+    
+    }    
     
 }
