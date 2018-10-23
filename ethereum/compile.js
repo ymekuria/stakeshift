9793d3c8f39b434aa185b2e0e751b6c68bb5b997
Yoni <ymekuria@gmail.com>
2018-10-23 12:46:06 -0700

    add paths for compile build and contract

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,3 +1,10 @@
 const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+// this deletes the folder and everything inside of it in the path
+fs.removeSync(buildPath);
+
+const contractPath = path.resolve(__dirname, 'contracts', 'StakShift.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
