c149809c112ce80e638732cc2c77b0aaaf75d188
Yoni <ymekuria@gmail.com>
2018-10-24 14:16:42 -0700

    add contract deploy error handeling

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -2,25 +2,27 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { mnemonic, nodeEndpoint } = require('../config/keys');
 
-// const provider = new HDWalletProvider(mnemonic, nodeEndpoint);
 const compiledContract = require('./build/StakeShift.json');
 
-console.log('compiledContract: ', compiledContract.bytecode);
 const provider = new HDWalletProvider(mnemonic, nodeEndpoint);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from account', accounts[0]);
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
 
-  const contractInstance = await new web3.eth.Contract(
-    JSON.parse(compiledContract.interface)
-  )
+    const contractInstance = await new web3.eth.Contract(
+      JSON.parse(compiledContract.interface)
+    )
 
-    .deploy({ data: compiledContract.bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+      .deploy({ data: compiledContract.bytecode })
+      .send({ gas: '3000000', from: accounts[0] });
 
-  console.log('Contract deployed to', contractInstance.options.address);
+    console.log('Contract deployed to', contractInstance.options.address);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 deploy();
