03a81eb274da86c5aa1b93a07af68f096d275d9f
Yoni <ymekuria@gmail.com>
2018-10-24 00:31:42 -0700

    deploy contract

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -4,6 +4,8 @@ const { mnemonic, nodeEndpoint } = require('../config/keys');
 
 // const provider = new HDWalletProvider(mnemonic, nodeEndpoint);
 const compiledContract = require('./build/StakeShift.json');
+
+console.log('compiledContract: ', compiledContract.bytecode);
 const provider = new HDWalletProvider(mnemonic, nodeEndpoint);
 const web3 = new Web3(provider);
 
@@ -13,7 +15,12 @@ const deploy = async () => {
 
   const contractInstance = await new web3.eth.Contract(
     JSON.parse(compiledContract.interface)
-  );
+  )
+
+    .deploy({ data: compiledContract.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', contractInstance.options.address);
 };
 
 deploy();
