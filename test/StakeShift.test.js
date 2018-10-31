c7c5294a44144255e987554b0a3683eab81731a6
Yoni <ymekuria@gmail.com>
2018-10-31 15:54:54 -0700

    test file scaffolding

---
+++ test/StakeShift.test.js
@@ -0,0 +1,14 @@
+assert = require('assert');
+ganache = require('ganache-cli');
+
+const compliedContract = require('../ethereum/build/StakeShift.json');
+
+let accounts;
+
+beforeEach(async () => {});
+
+describe('StakeShift', () => {
+  it('deploys a contract', () => {
+    assert(false);
+  });
+});
