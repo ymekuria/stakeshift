e150f886dc26e9aeba9c24cabd9fda83d738430d
Yoni <ymekuria@gmail.com>
2018-11-01 13:56:14 -0700

    can deploy to local ganache test network

--- test/StakeShift.test.js
+++ test/StakeShift.test.js
@@ -1,14 +1,26 @@
 assert = require('assert');
 ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
 
 const compliedContract = require('../ethereum/build/StakeShift.json');
 
 let accounts;
+let stakeShift;
+const contractJSONInterface = JSON.parse(compliedContract.interface);
 
-beforeEach(async () => {});
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  // deploying the contract to a ganache local test network
+  stakeShift = await new web3.eth.Contract(contractJSONInterface)
+    .deploy({ data: compliedContract.bytecode })
+    .send({ from: accounts[0], gas: '2000000' });
+});
 
 describe('StakeShift', () => {
   it('deploys a contract', () => {
-    assert(false);
+    console.log('Stake Shift', stakeShift.options.address);
+    assert.ok(stakeShift.options.address);
   });
 });
