21c48dee4258bee4d5f5471ca6caf23384c51802
Yoni <ymekuria@gmail.com>
2018-11-09 16:41:04 -0800

    add react-router react-drizzle

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import StakeShift from '../utils/ethereum/stakeShift';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 class App extends Component {
-  async componentDidMount() {
-    const stakeShift = await StakeShift(
-      '0xa53b887a9dbdc3bf75ce0bbf1fcd74158a1a1277'
-    );
-    console.log('stakeShift: ', stakeShift);
-    console.log('test');
-  }
   render() {
-    return <div>Hello World</div>;
+    return (
+      <BrowserRouter>
+        <div>Hello World</div>
+      </BrowserRouter>
+    );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { DrizzleContext } from 'drizzle-react';
 import App from './components/App';
-// import { Drizzle, generateStore } from 'drizzle';
 import drizzleOptions from './utils/drizzleOptions';
-// console.log('stakeShift: ', StakeShift);
-// const options = {
-//   contracts: [StakeShift]
-// };
 
-// const drizzleStore = generateStore(options);
-// const drizzle = new Drizzle(options, drizzleStore);
-
-// console.log('drizzle: ', drizzleOptions);
 ReactDOM.render(
   <DrizzleContext.Provider drizzle={drizzleOptions}>
     <App />
@@ -20,15 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 ReactDOM.render(<App />, document.getElementById('root'));
-// import { Drizzle, generateStore } from 'drizzle';
-// import StakeShift from './ethereum/build/StakeShift.json';
-
-// console.log('stakeShift: ', StakeShift);
-// const options = {
-//   contracts: [StakeShift]
-// };
-
-// const drizzleStore = generateStore(options);
-// const drizzle = new Drizzle(options, drizzleStore);
-
-// export default drizzle;
