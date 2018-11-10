748979c6c001ed44493f65cb783a98a8649a1555
Yoni <ymekuria@gmail.com>
2018-11-09 16:53:31 -0800

    add header component boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Header';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>Hello World</div>
+        <div>
+          <Header />
+        </div>
       </BrowserRouter>
     );
   }
