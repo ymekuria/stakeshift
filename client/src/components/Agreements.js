53eceeae6032f46b08e0851469b38d746d765458
Yoni <ymekuria@gmail.com>
2019-01-10 18:30:12 -0800

    connect agreements method to drizzleState

--- client/src/components/Agreements.js
+++ client/src/components/Agreements.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import withDrizzle from '../utils/withDrizzle';
 
 class Agreements extends Component {
+  state = { dataKey: null };
   async componentDidMount() {
     const { drizzle, drizzleState } = this.props;
 
-    const contract = drizzle.contracts.StakeShift;
+    // connects drizzle to watch for changes on agreements method on smart contract and add to drizzleState
+    const dataKey = drizzle.contracts.StakeShift.methods.agreements.cacheCall(
+      drizzleState.accounts[0]
+    );
 
-    const agreements = await contract.methods
-      .agreements(drizzleState.accounts[0])
-      .call();
-    console.log('agreements ', agreements);
+    this.setState({ dataKey });
   }
+
   render() {
-    return <div>Agreements</div>;
+    const { StakeShift } = this.props.drizzleState.contracts;
+    // use the data keyto get contract state from Drizzle
+    const agreements = StakeShift.agreements[this.state.dataKey];
+    // display values if they exist
+    return <div>Agreements{agreements && agreements.value.description}</div>;
   }
 }
 
