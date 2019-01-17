e2254aefefb76f058c07cd116090854c9fa71187
Yoni <ymekuria@gmail.com>
2019-01-17 14:01:20 -0800

    refactor ApprovalDisplay to class based component

--- client/src/components/ApprovalDisplay.js
+++ client/src/components/ApprovalDisplay.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Segment, Button } from 'semantic-ui-react';
 
-const ApprovalDisplay = props => {
-  console.log(props);
-  return (
-    <Segment.Group horizontal>
-      <Segment basic={true}>
-        <Button
-          primary
-          icon="add circle"
-          content={`${props.currentUserParty} Approve`}
-        />
-      </Segment>
-      <Segment color="green">{`Pending ${
-        props.counterParty
-      } Approval`}</Segment>
-    </Segment.Group>
-  );
-};
-
+class ApprovalDisplay extends Component {
+  render() {
+    return (
+      <Segment.Group horizontal>
+        <Segment basic={true}>
+          <Button
+            primary
+            icon="add circle"
+            content={`${this.props.currentUserParty} Approve`}
+          />
+        </Segment>
+        <Segment color="green">{`Pending ${
+          this.props.counterParty
+        } Approval`}</Segment>
+      </Segment.Group>
+    );
+  }
+}
 export default ApprovalDisplay;
