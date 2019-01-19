77b5621decb5e4324e92254618684ffe63396532
Yoni <ymekuria@gmail.com>
2019-01-19 01:46:53 -0800

    create currentUser data shape in state

--- client/src/actions/currentUserActions.js
+++ client/src/actions/currentUserActions.js
@@ -1,5 +1,21 @@
 import { GET_CURRENT_USER } from './types';
 
-export const getCurrentUser = agreement => {
-  return { type: GET_CURRENT_USER, payload: agreement };
+export const getCurrentUser = (currentUserAddress, agreements) => {
+  const currentUserParty = currentUserAddress === buyer ? 'buyer' : 'seller';
+
+  const counterParty = currentUserParty === 'buyer' ? 'seller' : 'buyer';
+  const counterPartyAddress = agreements[counterParty];
+
+  const currentUserApproved = agreements[`${currentUserParty}Approved`];
+
+  return {
+    type: GET_CURRENT_USER,
+    payload: {
+      currentUserParty,
+      currentUserAddress,
+      currentUserApproved,
+      counterParty,
+      counterPartyAddress
+    }
+  };
 };
